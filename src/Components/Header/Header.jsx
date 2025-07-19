f675c1e72e804be437c2cea7cbd18e84992d9698
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-07-20 00:23:44 +0600

    Fixed header padding size

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
     }
 
     return (
-        <div className="navbar bg-gradient-to-l from-[#FFF2F2] to-[#A9B5DF] shadow-sm p-7  fixed z-50 top-0">
+        <div className="navbar bg-gradient-to-l from-[#FFF2F2] to-[#A9B5DF] shadow-sm p-5 fixed z-50 top-0">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
